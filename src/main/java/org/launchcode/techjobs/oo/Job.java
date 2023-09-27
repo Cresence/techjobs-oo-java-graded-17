f48dbb062013b1763bd6ccd9f508abefc077de32
Cresence <devharris93@gmail.com>
2023-09-26 21:40:45 -0400

    Added unique IDs to Job Class and initialized other fields.

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -17,8 +17,39 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    /* This constructor lists the id as a static integer. Subsequent objects will initialize with iterated unique IDs??? */
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        /* Calls the previous Constructor? Does this initialize id??? */
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
+
+    @Override
+    public boolean equals(Object comparedObj) {
+        if (this == comparedObj) return true;
+
+        if (comparedObj == null) return false;
+
+        if (getClass() != comparedObj.getClass()) return false;
+
+        // Return for more comparison after Getters/Setters added
+        return true;
+
+    }
+
+
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
